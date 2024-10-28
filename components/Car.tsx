5d7372d7d684150e39204a788211f777ff648b68
LeviNoppers <levinoppers69@gmail.com>
2024-10-28 15:43:42 +0100

    .

--- components/Car.tsx
+++ components/Car.tsx
@@ -2,22 +2,38 @@ import styles from "../styles/components/Car.module.scss";
 import { useRouter } from "next/router";
 import Image from "next/image";
 
-interface pages {
+interface Page {
   label: string;
   src: string;
   value: string;
 }
 
-const Car = ({ carData }) => {
+interface CarProps {
+  make: string;
+  model: string;
+  fuel_type: string;
+  year: number;
+  hp: number;
+  seats: number;
+  weight: number;
+  engine_type: string;
+  image: string;
+}
 
-  const pages: pages[] = [
-    { label: 'Year', src: '/car/clock.png', value: String(carData.year)},
-    { label: 'Horsepower', src: '/car/engine.png', value: String(carData.hp) + ' Hp' },
-    { label: 'Weight', src: '/car/weight.png', value: String(carData.weight) + ' Kg'},
-    { label: 'Seats', src: '/car/car-seat.png', value: String(carData.seats)},
-    { label: 'Engine type', src: '/car/piston.png', value: String(carData.engine_type)},
-    { label: 'Fuel type', src: '/car/fuel.png', value: carData.fuel_type}
-  ]
+interface CarComponentProps {
+  carData: CarProps;
+}
+
+const Car: React.FC<CarComponentProps> = ({ carData }) => {
+  const pages: Page[] = [
+    { label: 'Year', src: '/car/clock.png', value: String(carData.year) },
+    { label: 'Horsepower', src: '/car/engine.png', value: `${carData.hp} Hp` },
+    { label: 'Weight', src: '/car/weight.png', value: `${carData.weight} Kg` },
+    { label: 'Seats', src: '/car/car-seat.png', value: String(carData.seats) },
+    { label: 'Engine type', src: '/car/piston.png', value: carData.engine_type },
+    { label: 'Fuel type', src: '/car/fuel.png', value: carData.fuel_type }
+  ];
+
   return (
     <div className={styles.car}>
       <Image alt={carData.model} src={carData.image} width={200} height={120} />
@@ -27,21 +43,20 @@ const Car = ({ carData }) => {
         </span>
       </header>
       <div>
-        {/*add object for items and map [0, 2] and [3, 5]  */}
         <ul>
-      {pages.slice(0, 3).map((page: any, index: number) => (
-        <div aria-label="multi" key={index}>
-          <Image alt={page.label} src={page.src} width={20} height={20} />
-          <li>{page.value}</li>
-        </div>
-      ))}
-    </ul>
+          {pages.slice(0, 3).map((page, index) => (
+            <li key={index} aria-label="multi">
+              <Image alt={page.label} src={page.src} width={20} height={20} />
+              {page.value}
+            </li>
+          ))}
+        </ul>
         <ul>
-          {pages.slice(3, 6).map((page: any, index: number) => (
-            <div aria-label="multi" key={index}>
-            <Image alt={page.label} src={page.src} width={20} height={20} />
-            <li>{page.value}</li>
-          </div>
+          {pages.slice(3, 6).map((page, index) => (
+            <li key={index} aria-label="multi">
+              <Image alt={page.label} src={page.src} width={20} height={20} />
+              {page.value}
+            </li>
           ))}
         </ul>
       </div>
